598494e049c25930771271a867d9274f8e3d88e2
sungbumchoi <46594664+sungbumchoi@users.noreply.github.com>
2024-01-04 23:35:31 +0900

    섹션 3. 영속성 관리 - 내부동작방식
    - 영속성 컨텍스트
    - 플러시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,8 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import static javax.print.attribute.standard.MediaSizeName.A;
+
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -22,11 +24,41 @@ public class JpaMain {
 //
 //            em.persist(member);
             // 조회
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember = " + findMember.getId());
-            System.out.println("findMember = " + findMember.getName());
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember = " + findMember.getId());
+//            System.out.println("findMember = " + findMember.getName());
             // 수정
-            findMember.setName("helloJPA");
+//            findMember.setName("helloJPA");
+
+            //객체를 생성한 상태(비영속)
+//            Member member = new Member();
+//            member.setId(100L);
+//            member.setName("회원1");
+
+            //영속
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member);
+//            System.out.println("=== AFTER ===");
+
+            // 1차캐시
+//            Member findMember1 = em.find(Member.class, 100L);
+//            Member findMember2 = em.find(Member.class, 100L);
+
+            // 동일성 보장
+//            System.out.println("result = " + (findMember1 == findMember2));
+
+            // 트랜잭션을 지원하는 쓰기 지연
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+//            System.out.println("================================");
+
+            // 변경감지
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
+            System.out.println("================================");
 
             tx.commit();
         } catch (Exception e) {
