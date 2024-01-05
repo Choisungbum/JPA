407bf8fb56624204aeed80214691efd53367e246
sungbumchoi <46594664+sungbumchoi@users.noreply.github.com>
2024-01-06 01:41:47 +0900

    섹션 4. 엔티티 매핑
     - 필드와 컬럼 매핑
     - 요구사항 분석과 기본 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,33 +1,36 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 // 처음 로딩될 떄 @Entity를 작성해줘야 JPA를 사용하는 클래스인지 알 수 있다.
 @Entity
-//@Table(name = "USER") // 매핑할 테이블의 이름을 적어줌
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 1)
 public class Member {
     // DB PK와 패핑
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    private String name;
-
-    public Member() {
-    }
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    // @Column(name = "name") : 테이블안의 컬럼명을 매핑
+    @Column(name = "name", nullable = false)
+    private String username;
+//
+//    private Integer age;
+//    // @Enumerated(EnumType.STRING) : enum 타입으로 받을 수 있음
+//    @Enumerated(EnumType.STRING)
+//    private RoleType roleType;
+//    // @Temporal(TemporalType.TIMESTAMP) : DB는 Date, Time, Timestamp 세가지 날짜 형식을 가짐
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//    // @Lob : CLOB,BLOB 등 크기가 큰 데이터를 다룬다 (문자 타입일 경우 기본 CLOB)
+//    @Lob
+//    private String description;
+//
     public Long getId() {
         return id;
     }
@@ -35,4 +38,52 @@ public class Member {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+//
+//    public Integer getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(Integer age) {
+//        this.age = age;
+//    }
+//
+//    public RoleType getRoleType() {
+//        return roleType;
+//    }
+//
+//    public void setRoleType(RoleType roleType) {
+//        this.roleType = roleType;
+//    }
+//
+//    public Date getCreatedDate() {
+//        return createdDate;
+//    }
+//
+//    public void setCreatedDate(Date createdDate) {
+//        this.createdDate = createdDate;
+//    }
+//
+//    public Date getLastModifiedDate() {
+//        return lastModifiedDate;
+//    }
+//
+//    public void setLastModifiedDate(Date lastModifiedDate) {
+//        this.lastModifiedDate = lastModifiedDate;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
 }
---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop;
+
+import hellojpa.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            // 실제 동장 코드 작성
+            em.close();
+        }
+
+        // 실제 애플리케이션 종료시 매니저 팩토리 닫기
+        emf.close();
+    }
+
+}
