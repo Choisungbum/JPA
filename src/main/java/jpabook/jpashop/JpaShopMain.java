21eca64d6843ded2a5dbfe20d80846b0b73d2dc6
sungbumchoi <46594664+sungbumchoi@users.noreply.github.com>
2024-01-07 22:32:51 +0900

    섹션 5. 연관관게 매핑 기초
    - 실전 예제 2 - 연관관계 매핑 시작

---
+++ src/main/java/jpabook/jpashop/JpaShopMain.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaShopMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Order order = new Order();
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            em.persist(orderItem);
+//            order.addOrderItem(order);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            // 실제 동장 코드 작성
+            em.close();
+        }
+
+        // 실제 애플리케이션 종료시 매니저 팩토리 닫기
+        emf.close();
+    }
+}
---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,66 @@
+package jpabook.jpashop.domain;
+
+import hellojpa.Member;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order") // One(현재 객체)ToMany(연관된 객체), mappedBy = order -> 연관관계에있는 객체에서 현재객체의 name
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    // 편의 메서드
+    private void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+
+    private LocalDateTime orderdate;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+
+    public LocalDateTime getOrderdate() {
+        return orderdate;
+    }
+
+    public void setOrderdate(LocalDateTime orderdate) {
+        this.orderdate = orderdate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
